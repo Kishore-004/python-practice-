b3f30fc84e294df0901024fa56b4f3eb13f7de8a
KISHORE <152643633+Kishore-004@users.noreply.github.com>
2025-01-27 21:12:03 +0530

    Add files via upload

---
+++ max of three number.py
@@ -0,0 +1,5 @@
+num1 = int(input("enter number 1 :"))
+num2 = int(input("enter number 2 :"))
+num3 = int(input("enter number 3 :"))
+result = max(num1,num2,num3)
+print("the maximum number is ",result)
